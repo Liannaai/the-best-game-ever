e502dd4301715b3f707288aabdc7899d119fdd2b
Liannaai <73580732+Liannaai@users.noreply.github.com>
2020-10-28 17:25:09 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+game.splash("The Parallel Universe ")
+game.showLongText("Introduction", DialogLayout.Top)
+game.showLongText("A hooded boy walked out of a blue house and looked up. He seemed to be waiting for something. After a little while a circle appeared on top of him. \"Are you ready? The others are waiting for you inside. The maze is done and the portal has been successfully hidden and blocked. The new ones will have a much harder time in the maze. We hid the map. Remember if they cut off all three of your tails you and the outhers are dead. But with the monsters oh, then good luck.\"said a dark and evil voice. Excellent, Excellent, If they make it we will have more to deal with", DialogLayout.Full)
+game.showLongText("", DialogLayout.Full)
